d680cf90de365068087f8057b29954b6fffbb7c2
Roman Vabishchevych <r@vabishchevych.com>
2021-11-23 22:38:54 +0200

    create components: Modal, ModalAuth

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 import Dropdown from './components/Dropdown';
-import './App.css';
 import Button from './components/Button';
+import ModalAuth from './components/ModalAuth';
+import './App.css';
 
 const DROPDOWN_DUMMY_OPTIONS = [
   { label: 'Item 1', value: 1 },
@@ -11,12 +12,22 @@ const DROPDOWN_DUMMY_OPTIONS = [
 
 function App() {
   const [ dropdownValue, setDropdownValue ] = useState(DROPDOWN_DUMMY_OPTIONS[0].value)
+  const [ isOpenAuth, setIsOpenAuth ] = useState(true);
+  const [ currentUser, setCurrentUser ] = useState();
+
+  const handleAuthSubmit = (user) => {
+    setIsOpenAuth(false);
+    setCurrentUser(user)
+  }
+  
   return (
     <div className="App">
       <Button onClick={() => console.log('click')} label="ImButton"/>
       <Dropdown options={DROPDOWN_DUMMY_OPTIONS} 
         value={dropdownValue}
         onChange={(e) => setDropdownValue(e.target.value)} />
+      Current user: {currentUser}
+      <ModalAuth open={ isOpenAuth } onSubmit={ handleAuthSubmit } />
     </div>
   );
 }
